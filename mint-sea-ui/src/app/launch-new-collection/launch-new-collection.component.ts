7348f71704798d091d647f057d26753316b528c6
Anand Sagar <winsome.ghost@gmail.com>
2021-10-24 16:32:13 +0530

    progress form

--- mint-sea-ui/src/app/launch-new-collection/launch-new-collection.component.ts
+++ mint-sea-ui/src/app/launch-new-collection/launch-new-collection.component.ts
@@ -11,6 +11,9 @@ export class LaunchNewCollectionComponent implements OnInit {
   showForPublicPreSale: boolean = false;
   placeHolderImg: string = "assets/img/placeholder.png";
   validateForm: string = "";
+  showStep1: string = "show"; showStep2: string = "hide"; showStep3: string = "hide"; showStep4: string = "hide"; showStep5: string = "hide";
+  classType2: any; classType3: any; classType4: any; classType5: any;
+  widthValue: string = "20";
 
   formModel: any = {
     collectionName: "",
@@ -61,6 +64,42 @@ export class LaunchNewCollectionComponent implements OnInit {
     // show error message in UI
     this.validateForm = "was-validated";
     console.log(this.formModel);
+  }
+
+  selectTab(oEvent: any) {
+    switch (oEvent.textContent) {
+      case "Step 1":
+        this.showStep1 = "show";
+        this.widthValue = "20";
+        this.showStep2 = this.showStep3 = this.showStep4 = this.showStep5 = "hide";
+        break;
+      case "Step 2":
+        this.showStep2 = "show";
+        this.classType2 = "active";
+        this.widthValue = "40";
+        this.showStep1 = this.showStep3 = this.showStep4 = this.showStep5 = "hide";
+        break;
+      case "Step 3":
+        this.showStep3 = "show";
+        this.classType3 = "active";
+        this.widthValue = "60";
+        this.showStep2 = this.showStep1 = this.showStep4 = this.showStep5 = "hide";
+        break;
+      case "Step 4":
+        this.showStep4 = "show";
+        this.classType4 = "active";
+        this.widthValue = "80";
+        this.showStep2 = this.showStep3 = this.showStep1 = this.showStep5 = "hide";
+        break;
+      case "Step 5":
+        this.showStep5 = "show";
+        this.classType5 = "active";
+        this.widthValue = "100";
+        this.showStep2 = this.showStep3 = this.showStep4 = this.showStep1 = "hide";
+        break;
+      default:
+        break;
+    }
   }
 
 }
